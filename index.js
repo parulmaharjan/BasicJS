676e0ef432097c77f8f26fb46bb333b7b1ecea72
parul <parulmhrzn@gmail.com>
2023-05-10 14:10:36 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// var x=90;
+// console.log(x);
+// x=120;
+// console.log(x);
+// var x=10-5;
+// console.log(x);
+// for LET
+// let x=20;
+// console.log(x);
+// x=30;
+// console.log(x);
+// let x=5+3;
+// console.log(x);
+const x=170;
+console.log(x);
+x=145;
+console.log(x);
