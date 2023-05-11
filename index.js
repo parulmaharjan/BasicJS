a62e72bd13239fda334707259a4691a7ac5ae1ea
parul <parulmhrzn@gmail.com>
2023-05-11 16:53:18 +0545

    array, array methods and loop

--- index.js
+++ index.js
@@ -1,189 +1,273 @@
-// var x=90;
-// console.log(x);
-// x=120;
-// console.log(x);
-// var x=10-5;
-// console.log(x);
-// for LET
-// let x=20;
-// console.log(x);
-// x=30;
-// console.log(x);
-// let x=5+3;
+// // var x=90;
 // // console.log(x);
-// const x=170;
-// console.log(x);
-// x=145;
-// console.log(x);
-//string method
-//const result="   my name is lord ";
-//console.log(result.length);
+// // x=120;
+// // console.log(x);
+// // var x=10-5;
+// // console.log(x);
+// // for LET
+// // let x=20;
+// // console.log(x);
+// // x=30;
+// // console.log(x);
+// // let x=5+3;
+// // // console.log(x);
+// // const x=170;
+// // console.log(x);
+// // x=145;
+// // console.log(x);
+// //string method
+// //const result="   my name is lord ";
+// //console.log(result.length);
 
-//concat
-//console.log(result.concat("hare krishna"));
+// //concat
+// //console.log(result.concat("hare krishna"));
 
-//substring
-//console.log(result.substring(1,4));//index no. linxa
-//console.log(result.substr(1,4));//lenght linxa
+// //substring
+// //console.log(result.substring(1,4));//index no. linxa
+// //console.log(result.substr(1,4));//lenght linxa
 
-//replace
-// // console.log(result.replace("my","his"));
-// // //includes=simply reads boolean and return value
-// // console.log(result.includes("lord"));
+// //replace
+// // // console.log(result.replace("my","his"));
+// // // //includes=simply reads boolean and return value
+// // // console.log(result.includes("lord"));
 
-// //charat=returns character
-// console.log(result.charAt(8));
+// // //charat=returns character
+// // console.log(result.charAt(8));
 
-//indexof
-//console.log(result.indexOf("l"));
+// //indexof
+// //console.log(result.indexOf("l"));
 
-//last index of
-//console.log(result.lastIndexOf("r"));
-//console.log(result.split(" "));
-// lower case and upper case
-// console.log(result.toLowerCase());
-// console.log(result.toUpperCase());
+// //last index of
+// //console.log(result.lastIndexOf("r"));
+// //console.log(result.split(" "));
+// // lower case and upper case
+// // console.log(result.toLowerCase());
+// // console.log(result.toUpperCase());
 
-//trim
-//console.log(result.trim());
+// //trim
+// //console.log(result.trim());
 
-//statement(if else statement)
+// //statement(if else statement)
 
-// const email="parulmhrzn@gmail.com";
-// const password="parul";
-// if (email==="parulmhrzn@gmail.com"&& password==="parul") {
-//     console.log("login sucessfully");
+// // const email="parulmhrzn@gmail.com";
+// // const password="parul";
+// // if (email==="parulmhrzn@gmail.com"&& password==="parul") {
+// //     console.log("login sucessfully");
 
-// }
-//     else{
-//         console.log("invalid credentials");
-//     }
+// // }
+// //     else{
+// //         console.log("invalid credentials");
+// //     }
 
-//simple arthemetic
+// //simple arthemetic
 
-// let x=10;
-// if(x>15){
-//     console.log("x is greater than 15");
-// }
-// else if(x>5){
-//     console.log("x is greater than 5 but not equal to 15 ");
+// // let x=10;
+// // if(x>15){
+// //     console.log("x is greater than 15");
+// // }
+// // else if(x>5){
+// //     console.log("x is greater than 5 but not equal to 15 ");
 
-// }
-// else{
-//     console.log("x is not greater than 15");
-// }
+// // }
+// // else{
+// //     console.log("x is not greater than 15");
+// // }
 
-// let weather="rainy";
-// if(weather==="sunny"){
-//     console.log("its a sunny day!!!");
-// }
-// else if(weather==="rainy"){
-//     console.log("its rainy day. carry your umbrella!");
-// }
-// else if(weather==="winter"){
-//     console.log("its cold outside");
-// }
-// else if(weather==="cloudy"){
-//     console.log("cloudy weather");
-// }
-// else{
-//     console.log("invalid!!");
-// }
+// // let weather="rainy";
+// // if(weather==="sunny"){
+// //     console.log("its a sunny day!!!");
+// // }
+// // else if(weather==="rainy"){
+// //     console.log("its rainy day. carry your umbrella!");
+// // }
+// // else if(weather==="winter"){
+// //     console.log("its cold outside");
+// // }
+// // else if(weather==="cloudy"){
+// //     console.log("cloudy weather");
+// // }
+// // else{
+// //     console.log("invalid!!");
+// // }
 
-//switch statement
-// let grade = "A";
-// switch (grade) {
-//   case "B":
-//     console.log("you got B. nice");
+// //switch statement
+// // let grade = "A";
+// // switch (grade) {
+// //   case "B":
+// //     console.log("you got B. nice");
 
-//     break;
+// //     break;
 
-//   case "B+":
-//     console.log("you got B+. nice");
+// //   case "B+":
+// //     console.log("you got B+. nice");
 
-//     break;
-//   case "C":
-//     console.log("you got C. ");
-//     break;
+// //     break;
+// //   case "C":
+// //     console.log("you got C. ");
+// //     break;
 
-//   case "C+":
-//     console.log("you got C+. ");
+// //   case "C+":
+// //     console.log("you got C+. ");
 
-//     break;
+// //     break;
 
-//   case "A+":
-//     console.log("you got A+. excillent");
+// //   case "A+":
+// //     console.log("you got A+. excillent");
 
-//     break;
-//   case "A":
-//     console.log("you got A. very good");
+// //     break;
+// //   case "A":
+// //     console.log("you got A. very good");
 
-//     break;
+// //     break;
 
-//   default:
-//     console.log("you failed");
-//     break;
-// }
+// //   default:
+// //     console.log("you failed");
+// //     break;
+// // }
+
+// // let month = "May";
+// // switch (month) {
+// //     case "Jan":
+// //     case "Feb":
+// //     case "March":
+// //         console.log("the month is in Q1");
+// //         break;
+
+// //     case "april":
+// //     case "May":
+// //     case "June":
+// //         console.log("the month is in Q2");
+// //         break;
+// //     case "July":
+// //     case "august":
+// //     case "sept":
+// //         console.log("the month is in Q3");
+// //         break;
+// //     case "Jan":
+// //     case "Oct":
+// //     case "Nov":
+// //     case "Dec":
+
+// //         console.log("the month is in Q4");
+// //         break;
+
+// //     default:
+// //         console.log("vetiyena")
+// //         break;
+// // }
+
+// // let weather = "Rainy";
+// // switch (weather) {
+// //     case "sunny":
+// //         console.log("sunny weather");
+
+// //         break;
+// //     case "windy":
+// //         console.log("windy weather");
+
+// //         break;
+// //     case "Rainy":
+// //         console.log("Rainy weather");
+
+// //         break;
+// //     case "cloudy":
+// //         console.log("cloudy weather");
+
+// //         break;
+
+// //     default:
+// //         console.log("no weather updates");
+// //         break;
+// // }
+// //DOM(Document object model)
+// // const title=document.getElementById("mytitle");
+// // title.innerHTML="Dom manipulation";
+// // title.style.backgroundColor="red";
+// // title.style.color="white";
+// // title.style.cursor="pointer"
+// // console.log(title);
+
+// // //practise
+// // let weather = prompt("enter your favourite weather:");
+// // switch (weather) {
+// //   case "cloudy":
+// //     console.log("congrats its cloudy today");
+// //     break;
+// //   case "sunny":
+// //     console.log("congrats its sunny today");
+
+// //     break;
+// //   case "rainy":
+// //     console.log("congrats its rainy today");
+
+// //     break;
+// //   case "windy":
+// //     console.log("congrats its windy today");
+
+// //     break;
+// //   case "breezy":
+// //     console.log("congrats its cloudy today");
+
+// //     break;
+// //   case "cool":
+// //     console.log("congrats its cool today");
+
+// //     break;
+// //   case "dusky":
+// //     console.log("congrats its cloudy today");
+
+// //     break;
+// //   case "humid":
+// //     console.log("congrats its humid today");
+// //     break;
+
+// //   default:
+// //     console.log("sorry weather is not in your favour today");
+// //     break;
+// // }
 
 
-// let month = "May";
-// switch (month) {
-//     case "Jan":
-//     case "Feb":
-//     case "March":
-//         console.log("the month is in Q1");
-//         break;
+// //loop
+// //1)for loop
+// // for (let i = 1; i <=10; i++) {
+// //   console.log(i);
+// //   }
 
-//     case "april":
-//     case "May":
-//     case "June":
-//         console.log("the month is in Q2");
-//         break;
-//     case "July":
-//     case "august":
-//     case "sept":
-//         console.log("the month is in Q3");
-//         break;
-//     case "Jan":
-//     case "Oct":
-//     case "Nov":
-//     case "Dec":
 
-//         console.log("the month is in Q4");
-//         break;
+// //sum of 10 positive number
+// // let sum=0;
+// // for (let index = 1; index <= 10; index++) {
+// //   sum+=index; //sum=sum+index
+// //   //const element = array[index];
+  
+// // }
+// // console.log(sum);
+// //while loop
+// // let i=0;
+// // let sum=0;
+// // while (i<=10) {
+// //   sum+=i;
+  
+// //   i++;}
+// //   console.log(sum);
 
-//     default:
-//         console.log("vetiyena")
-//         break;
-// }
+// //dowhile loop
+// let i=0;
+// let sum=0;
+// do {
+//  sum+=i;
+//   i++;
+// } while (i<=10);
+// console.log(sum);
 
-// let weather = "Rainy";
-// switch (weather) {
-//     case "sunny":
-//         console.log("sunny weather");
+//arrray
+// const array=[0,1,2,3,4,5,6,7,8,9];
+// const array1=["ram","shyam","hari"];
+// console.log(array[2]);
+// console.log(array1)
 
-//         break;
-//     case "windy":
-//         console.log("windy weather");
+// const array=[1,2,3,4,5,6,"Ram","shyam"];  //mix array i.e mixing data type
+// console.log(array);
 
-//         break;
-//     case "Rainy":
-//         console.log("Rainy weather");
-
-//         break;
-//     case "cloudy":
-//         console.log("cloudy weather");
-
-//         break;
-
-//     default:
-//         console.log("no weather updates");
-//         break;
-// }
-//DOM(Document object model)
-const title=document.getElementById("mytitle");
-title.innerHTML="Dom manipulation";
-title.style.backgroundColor="red";
-title.style.color="white";
-title.style.cursor="pointer"
-console.log(title);
+//array methods (basic array method);
+  
